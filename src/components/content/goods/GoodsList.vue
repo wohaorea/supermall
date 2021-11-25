c035876b66bed7e0e5f1fb74c5d717c69a2d47ef
wohaorea <1634756418@qq.com>
2021-11-25 17:04:18 +0800

    '首页已完成'

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" :goods-item="item" class="item"></goods-list-item>
+    {{goods}}
+  </div>
+</template>
+
+<script>
+  import GoodsListItem from './GoodsListItem'
+
+  export default {
+    name:"GoodsList",
+    components: {
+      GoodsListItem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+ .goods {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 5px;
+    justify-content: space-around;
+  }
+  .goods .item {
+    width: 48%;
+  }
+</style>
